784863533ecd71fe9db5d4e2b2028e75fc4c6006
juliannzhu <juju.zhu123@gmail.com>
2026-07-18 00:55:23 -0400

    reformat files

---
+++ CaneOS App/CaneOS App/CaneOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaneOS_AppApp.swift
+//  CaneOS App
+//
+//  Created by Juliann Zhu on 2026-07-18.
+//
+
+import SwiftUI
+
+@main
+struct CaneOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaneOS App/CaneOS Watch App Watch App/CaneOS_Watch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaneOS_Watch_AppApp.swift
+//  CaneOS Watch App Watch App
+//
+//  Created by Juliann Zhu on 2026-07-18.
+//
+
+import SwiftUI
+
+@main
+struct CaneOS_Watch_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
